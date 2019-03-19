9693b4bb80c894546a848e574603ef33a9864fc5
Remigio Vildacci <rvildacci@gmail.com>
2019-03-19 19:12:17 +0100

    setup Teslabattery Container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery';
 
-class App extends Component {
+  class App extends Component {
     render() {
-        return (
-          <div class="App">
-            <h2>Let's get started</h2>
-          </div>
-        );
+      return (
+        <div className="App">
+          <Header />
+          <TeslaBattery />
+        </div>
+      );
     }
-}
-
-export default App;
+  }
+  export default App;
